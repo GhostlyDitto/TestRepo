e7957de0e7af665239f0484b73800b0d7e96f664
AIE\s220220 <setonhiggins555@gmail.com>
2023-04-03 18:46:50 +0930

    Added FPS project

---
+++ FPS Tutorial/Assets/Scripts/HandleProjectile.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleProjectile : MonoBehaviour
+{
+    public float projectileLife;
+    private float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= projectileLife)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
